537c9345f8c98d282ddb5d38bb11b6590eb3b419
AKampani01 <anna1kamp@gmail.com>
2020-03-15 13:43:51 +0000

    css

--- DecodeCollective/js/contactJsElements.js
+++ DecodeCollective/js/contactJsElements.js
@@ -8,4 +8,9 @@ const bioChristosText = document.querySelector("#bioChristosText");
 
 bioAnna.addEventListener("click", function() {
     bioAnnaText.classList.toggle("hidden");
+    bioChristosText.classList.add("hidden");
+});
+bioChristos.addEventListener("click", function() {
+    bioChristosText.classList.toggle("hidden");
+    bioAnnaText.classList.add("hidden");
 });

49b1d8982491e0aa0c458b28fc069722b2f7ae8d
AKampani01 <anna1kamp@gmail.com>
2020-03-21 22:56:25 +0000

    map update

--- DecodeCollective/js/Map.js
+++ DecodeCollective/js/Map.js
@@ -3,29 +3,29 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYW5uYWthbXAiLCJhIjoiY2pxcGQ2dDJuMDBjbzQ0bnV0e
 var map = new mapboxgl.Map({
     container: 'map', // Container ID
     style: 'mapbox://styles/annakamp/ck7gkqyfj1srz1imrg3f1j9h5', // Map style to use
-    center: [-0.1019,51.5032], // Starting position [lng, lat]
-    zoom: 11, // Starting zoom level
+    center: [7.3072819,45.9083282], // Starting position [lng, lat]
+    zoom: 3.5, // Starting zoom level
     preserveDrawingBuffer: true,
     language: 'en-EN',
     //dragPan: false
 });
 
 
-//---------------- Geocoder  ---------------//
-var geocoder = new MapboxGeocoder({ // Initialize the geocoder
-    accessToken: mapboxgl.accessToken, // Set the access token
-    mapboxgl: mapboxgl, // Set the mapbox-gl instance
-    marker: false, // Do not use the default marker style
-    placeholder: 'Search for a place', // Placeholder text for the search bar
-    language: 'en',
-    limit: 1,
-    zoom: 15
+// //---------------- Geocoder  ---------------//
+// var geocoder = new MapboxGeocoder({ // Initialize the geocoder
+//     accessToken: mapboxgl.accessToken, // Set the access token
+//     mapboxgl: mapboxgl, // Set the mapbox-gl instance
+//     marker: false, // Do not use the default marker style
+//     placeholder: 'Search for a place', // Placeholder text for the search bar
+//     language: 'en',
+//     limit: 1,
+//     zoom: 15
 
-});
+// });
 
-// Add the geocoder to the map
+// // Add the geocoder to the map
+// map.addControl(geocoder);
 
-map.addControl(geocoder);
 map.addControl(new mapboxgl.ScaleControl({
     position: 'bottom-right'
 }));
@@ -40,11 +40,11 @@ map.on('load', function () {
         }
     });
 
-    // Listen for the `result` event from the Geocoder
-    // `result` event is triggered when a user makes a selection
-    geocoder.on('result', function (ev) {
-        map.getSource('single-point').setData(ev.result.geometry);
-    });
+    // // Listen for the `result` event from the Geocoder
+    // // `result` event is triggered when a user makes a selection
+    // geocoder.on('result', function (ev) {
+    //     map.getSource('single-point').setData(ev.result.geometry);
+    // });
 
     //---------------- Set map language
     map.setLayoutProperty('country-label', 'text-field', [
@@ -58,10 +58,21 @@ map.on('load', function () {
    
 });
 
+// disable map rotation using right click + drag
+map.dragRotate.disable();
+// disable map rotation using touch rotation gesture
+map.touchZoomRotate.disableRotation();
+
 // make a marker for each feature and add to the map
 let el = document.createElement('div');
   el.className = 'marker';
 
 new mapboxgl.Marker(el)
 .setLngLat([-0.11472,51.51299])
+.addTo(map);
+
+let elA = document.createElement('div');
+  elA.className = 'marker';
+new mapboxgl.Marker(elA)
+.setLngLat([23.7613095,37.9851516])
 .addTo(map);
--- DecodeCollective/js/projectsJSElements.js
+++
@@ -1,7 +0,0 @@
-//------------------------- BUTTON LISTENERS -------------------------//
-const projectMenuBtn = document.querySelector(".project-menu-toggle");
-const projectsOverlay = document.getElementById("projects-overlay");
-
-projectMenuBtn.addEventListener("click", function() {
-    projectMenuBtn.classList.toggle("project-menu-select");
-});

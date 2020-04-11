2d3c047ce72fb0b9db9b1851bf6c355cfef02866
ChristosCh <chrishatzak@gmail.com>
2020-04-11 17:24:15 +0100

    img

--- DecodeCollective/js/slideshow.js
+++ DecodeCollective/js/slideshow.js
@@ -1,13 +1,13 @@
-$("#slideshow > div:gt(0)").hide();
+// $("#slideshow > div:gt(0)").hide();
 
-setInterval(function() { 
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-},  3000);
+// setInterval(function() { 
+//   $('#slideshow > div:first')
+//     .fadeOut(1000)
+//     .next()
+//     .fadeIn(1000)
+//     .end()
+//     .appendTo('#slideshow');
+// },  3000);
 
 // const slideshowList = document.getElementById("slideshow");
 //  slideshowRotation();

46991dfe45bb57cdd70268506b97c533815caf18
ChristosCh <chrishatzak@gmail.com>
2020-04-12 11:40:22 +0100

    rawbot pic

--- DecodeCollective/js/slideshow.js
+++ DecodeCollective/js/slideshow.js
@@ -1,35 +1,9 @@
-// $("#slideshow > div:gt(0)").hide();
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
 
-// setInterval(function() { 
-//   $('#slideshow > div:first')
-//     .fadeOut(1000)
-//     .next()
-//     .fadeIn(1000)
-//     .end()
-//     .appendTo('#slideshow');
-// },  3000);
-
-// const slideshowList = document.getElementById("slideshow");
-//  slideshowRotation();
-
-// // function slideshowRotation(){
-// //     for(let i=0; i<slideshowList.length; i++){
-// //         slideshowList[i].classList.remove("hidden");
-// //         setTimeout(function(){
-// //              slideshowList[i].classList.add("hidden");
-            
-// //         }, 3000);
-// //     }
-// // }
-
-// function slideshowRotation(){
-//     setInterval(function(){
-//         let childNum=slideshowList.firstChild;
-//         childNum.fadeOut(1000);
-//         childNum.nextSibling.fadeIn(1000);
-//         slideshowList.removeChild(slideshowList.firstChild);
-//         slideshowList.appendChild(childNum);        
-
-//     },3000);
-
-// }
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);

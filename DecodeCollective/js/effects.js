292c2e494de733248edd416854fcfd7eb129c694
AKampani01 <anna1kamp@gmail.com>
2022-01-02 14:41:14 +0000

    (feat): project popup and about

---
+++ DecodeCollective/js/effects.js
@@ -0,0 +1,126 @@
+const config ={
+    "background": {
+      "color": {
+        "value": "#ffffff"
+      },
+      "position": "50% 50%",
+      "repeat": "no-repeat",
+      "size": "cover"
+    },
+    "fullScreen": {
+      "zIndex": 1
+    },
+    "detectRetina": false,
+    "fpsLimit": 30,
+    "interactivity": {
+      "events": {
+        "onClick": {
+          "mode": "push"
+        },
+        "onDiv": {
+          "selectors": "#repulse-div",
+          "mode": "repulse"
+        },
+        "onHover": {
+          "enable": true,
+          "mode": "bubble"
+        }
+      },
+      "modes": {
+        "bubble": {
+          "distance": 40,
+          "duration": 2,
+          "opacity": 8,
+          "size": 6
+        },
+        "grab": {
+          "distance": 400
+        },
+        "slow": {
+          "factor": 1,
+          "radius": 0
+        }
+      }
+    },
+    "particles": {
+      "color": {
+        "value": "#000000"
+      },
+      "links": {
+        "color": {
+          "value": "#000000"
+        },
+        "distance": 30,
+        "enable": true,
+        "opacity": 0.4
+      },
+      "move": {
+        "attract": {
+          "rotate": {
+            "x": 600,
+            "y": 1200
+          }
+        },
+        "enable": true,
+        "outModes": {
+          "default": "bounce",
+          "bottom": "bounce",
+          "left": "bounce",
+          "right": "bounce",
+          "top": "bounce"
+        },
+        "speed": 1
+      },
+      "number": {
+        "density": {
+          "area": 2000
+        },
+        "value": 200
+      },
+      "opacity": {
+        "value": {
+          "min": 0.05,
+          "max": 0.4
+        },
+        "animation": {
+          "enable": true,
+          "minimumValue": 0.05
+        }
+      },
+      "size": {
+        "random": {
+          "enable": true
+        },
+        "value": 1,
+        "animation": {
+          "speed": 40,
+          "minimumValue": 0.1
+        }
+      }
+    },
+    "polygon": {
+      "draw": {
+        "enable": true,
+        "stroke": {
+          "color": {
+            "value": "rgba(0,0,0,0.2)"
+          },
+          "width": 0.5,
+          "opacity": 0.2
+        }
+      },
+      "enable": true,
+      "inline": {
+        "arrangement": "equidistant"
+      },
+      "move": {
+        "radius": 10,
+        "type": "path"
+      },
+      "scale": 0.5,
+      "type": "inline",
+      "url": "../images/logo/logo.svg"
+    }
+  };
+
+tsParticles.load("tsparticles", config);
--- DecodeCollective/js/projectsJSElements.js
+++ DecodeCollective/js/projectsJSElements.js
@@ -1,21 +1,11 @@
-// const currentel = document.querySelector("#current-project");
-// const divider = document.querySelector("#divider");
-// const scrollbutton = document.querySelector(".scroll-arrow");
-// const footer = document.querySelector(".footer");
-// const prjctbtns = document.querySelectorAll('.project-button');
-// const scrolltxt = document.querySelector('#scroll-text');
-
-//scrollbutton.addEventListener('click', scrollToNextElement);
-//footer.addEventListener('click', scrollToNextElement);
-
 const navBarIcon = document.querySelector('.icon');
 navBarIcon.addEventListener('click', handleNavBar);
 
 const popup = document.querySelector('.popup');
 const popupX = document.querySelector(".x-btn");
-popupX.addEventListener('click', () => { 
-    popup.classList.add('hidden') 
-    popup.classList.remove('popup-open') 
+popupX.addEventListener('click', () => {
+    popup.classList.add('hidden')
+    popup.classList.remove('popup-open')
 });
 
 
@@ -48,82 +38,6 @@ function scrollToNextElement() {
 }
 
 
-
-//------------------------- WAIT TO LOAD THE WHOLE PAGE -------------------------//
-// let loader = document.getElementById('loader');
-// window.addEventListener("load", function (event) {
-//     loader.classList.remove('loading');
-//     loader.classList.add('loaded');
-//     document.body.classList.add('imgloaded');
-// });
-
-//------------------------- TRIGGER ANIMATIONS WHEN ELEMENT IS IN VIEWPORT -------------------------//
-// const numSteps = 20.0;
-// // Set things up
-// window.addEventListener("load", (event) => {
-//     boxElement = document.querySelectorAll(".show-on-scroll");
-//     for (let i = 0; i < boxElement.length; i++) {
-//         if (boxElement[i] != undefined) {
-//             createObserver(boxElement[i]);
-//         }
-//     }
-
-// }, false);
-
-
-// function createObserver(element) {
-//     let observer;
-
-//     let options = {
-//         root: null,
-//         rootMargin: "0px",
-//         threshold: buildThresholdList()
-//     };
-
-//     observer = new IntersectionObserver(handleIntersect, options);
-//     observer.observe(element);
-// }
-
-// function buildThresholdList() {
-//     let thresholds = [];
-//     let numSteps = 20;
-
-//     for (let i = 1.0; i <= numSteps; i++) {
-//         let ratio = i / numSteps;
-//         thresholds.push(ratio);
-//     }
-
-//     thresholds.push(0);
-//     return thresholds;
-// }
-
-// function handleIntersect(entries, observer) {
-//     entries.forEach((entry) => {
-//         if (entry.intersectionRatio >= 1) {
-//             currentel.innerText = entry.target.getAttribute('index');
-//             if (currentel.innerText == prjctbtns.length) {
-//                 scrollbutton.style.transform = 'rotate(270deg)';
-//                 scrolltxt.innerText = "Back To Top";
-//             }
-//             else {
-//                 scrollbutton.style.transform = 'rotate(90deg)';
-//                 scrolltxt.innerText = "Scroll";
-
-//             }
-//             // entry.target.classList.remove("slide-top");   
-
-//             divider.classList.remove("divider");
-//             setTimeout(function () {
-//                 // entry.target.classList.add("slide-top");   
-
-//                 divider.classList.add("divider");
-//             }, 200);
-//         }
-//     });
-// }
-
-
-
 //------------------------- GRID MOUSEOVER EFFECT -------------------------//
 const gridItems = [...document.querySelector('.grid').querySelectorAll('picture')];
 const polygon = document.querySelector('#polygon')
@@ -154,7 +68,7 @@ function hoverEffect(el) {
     polygon.style.width = el.lastElementChild.clientWidth.toString() + 'px';
     polygon.style.height = el.lastElementChild.clientHeight.toString() + 'px';
     const position = getPos(el)
-    polygon.style.top = position.y - scrollParent.scrollTop + "px";    
+    polygon.style.top = position.y - scrollParent.scrollTop + "px";
     polygon.style.left = position.x + "px";
     el.nextElementSibling.style.opacity = 1
     polygon.firstElementChild.style.backgroundColor = getRandomColor()
@@ -168,7 +82,7 @@ function hoverEffectTitle() {
 
 function hoverEffectClose() {
     if (!currentElement || !screenSize) return
-    currentElement.nextElementSibling.style.opacity = 0    
+    currentElement.nextElementSibling.style.opacity = 0
 }
 
 function getPos(el) {
@@ -219,3 +133,33 @@ function detectHover() {
     }
     else screenSize = true
 }
+
+function switchProject() {
+    document.addEventListener("click", ev => {
+        if (ev.target.classList.contains("next-project")) {            
+            popupX.click();
+            let index = gridItems.findIndex((x, i) => x.parentElement.href == currentElement.parentElement.href);
+            let nextEl;
+            if (index + 1 < gridItems.length) nextEl = gridItems[index + 1];
+            else nextEl = gridItems[0];
+            hoverEffect(nextEl);
+            openPopup();
+        }
+        else  if (ev.target.classList.contains("previous-project")) {
+            popupX.click();
+            let index = gridItems.findIndex((x, i) => x.parentElement.href == currentElement.parentElement.href);
+            let nextEl;
+            if (index - 1 >=0) nextEl = gridItems[index - 1];
+            else nextEl = gridItems[gridItems.length-1];
+            hoverEffect(nextEl);
+            openPopup();
+        }
+    })
+
+}
+
+switchProject();
+
+
+
+
